e39f965e75a6b02d254aaab4e5a6e78597da008c
BM-Mehmet <dt.mehmetcam@gmail.com>
2024-05-27 18:17:56 +0300

    Register eklendi.

---
+++ Register.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Register.aspx.cs" Inherits="StudentTracking.Register" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>E-posta Gönderme Formu</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server" class="container mt-5">
+        <div class="card">
+            <div class="card-body">
+                <h2 class="card-title">E-posta Gönderme Formu</h2>
+                <asp:Label ID="lblMessage" runat="server" Text="" ForeColor="Red"></asp:Label>
+                <div class="form-group">
+                    <label for="txtRecipient">Alıcı E-posta Adresi</label>
+                    <asp:TextBox ID="txtRecipient" runat="server" CssClass="form-control" placeholder="Alıcı E-posta Adresi"></asp:TextBox>
+                </div>
+                <asp:Button ID="btnSendEmail" runat="server" Text="Gönder" OnClick="btnSendEmail_Click" CssClass="btn btn-primary" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Register.aspx.cs
@@ -0,0 +1,134 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Services;
+using System;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace StudentTracking
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        //protected void btnSendEmail_Click(object sender, EventArgs e)
+        //{
+        //    string emailPassword = ConfigurationManager.AppSettings["EmailPassword"];
+
+        //    try
+        //    {
+        //        // SMTP sunucusu ve kimlik bilgilerini kullanarak SmtpClient oluşturma
+        //        SmtpClient smtpClient = new SmtpClient("smtp-mail.outlook.com", 587);
+        //        smtpClient.EnableSsl = true;
+        //        smtpClient.Credentials = new NetworkCredential("studenttracking@outlook.com", emailPassword); // Outlook adresiniz ve şifreniz
+
+        //        // E-posta mesajını oluşturma
+        //        MailMessage mailMessage = new MailMessage();
+        //        mailMessage.From = new MailAddress("studenttracking@outlook.com"); // Gönderen e-posta adresi
+        //        mailMessage.To.Add(txtRecipient.Text); // Alıcı e-posta adresi (kullanıcının girdiği adres)
+        //                                               // Rastgele 6 haneli bir sayı oluşturma
+        //        Random random = new Random();
+        //        int randomNumber = random.Next(100000, 999999); // 100000 ile 999999 arasında rastgele bir sayı oluşturur
+        //        string messageBody = "Şifreniz: " + randomNumber.ToString();
+        //        mailMessage.Body = messageBody; // E-posta içeriği
+
+        //        // E-postayı gönderme
+        //        smtpClient.Send(mailMessage);
+
+        //        // Başarılı bir şekilde gönderildiğinde kullanıcıya bilgi verme
+        //        lblMessage.Text = "E-posta başarıyla gönderildi!";
+        //        lblMessage.ForeColor = System.Drawing.Color.Green;
+        //        string recipient = txtRecipient.Text;
+
+        //        using (var db = new StudentTrackingDBEntities()) // Veritabanı bağlantısı
+        //        {
+        //            // Yeni öğrenci oluştur
+        //            var newStudent = new students
+        //            {
+        //                password = emailPassword,  
+        //            };
+
+        //            db.students.Add(newStudent); // Öğrenciyi veritabanına ekle
+        //            db.SaveChanges(); // Değişiklikleri kaydet
+
+        //        }
+
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // Hata durumunda kullanıcıya bilgi verme
+        //        lblMessage.Text = "E-posta gönderirken bir hata oluştu: " + ex.Message;
+        //        lblMessage.ForeColor = System.Drawing.Color.Red;
+        //    }
+        //}
+
+        protected void btnSendEmail_Click(object sender, EventArgs e)
+        {
+            string emailPassword = ConfigurationManager.AppSettings["EmailPassword"];
+
+            try
+            {
+                // SMTP sunucusu ve kimlik bilgilerini kullanarak SmtpClient oluşturma
+                SmtpClient smtpClient = new SmtpClient("smtp-mail.outlook.com", 587);
+                smtpClient.EnableSsl = true;
+                smtpClient.Credentials = new NetworkCredential("studenttracking@outlook.com", emailPassword); // Outlook adresiniz ve şifreniz
+
+                // E-posta mesajını oluşturma
+                MailMessage mailMessage = new MailMessage();
+                mailMessage.From = new MailAddress("studenttracking@outlook.com"); // Gönderen e-posta adresi
+                mailMessage.To.Add(txtRecipient.Text); // Alıcı e-posta adresi (kullanıcının girdiği adres)
+
+                // Rastgele 6 haneli bir sayı oluşturma
+                Random random = new Random();
+                int randomNumber = random.Next(100000, 999999); // 100000 ile 999999 arasında rastgele bir sayı oluşturur
+                string messageBody = "Şifreniz: " + randomNumber.ToString();
+                mailMessage.Body = messageBody; // E-posta içeriği
+
+                // E-postayı gönderme
+                smtpClient.Send(mailMessage);
+
+                // Başarılı bir şekilde gönderildiğinde kullanıcıya bilgi verme
+                lblMessage.Text = "E-posta başarıyla gönderildi!";
+                lblMessage.ForeColor = System.Drawing.Color.Green;
+                string recipient = txtRecipient.Text;
+
+                using (var db = new StudentTrackingDBEntities()) // Veritabanı bağlantısı
+                {
+                    // Veritabanında belirli bir e-posta adresine sahip kullanıcıyı bul
+                    var existingStudent = db.students.FirstOrDefault(s => s.email == recipient);
+
+                    if (existingStudent == null) // Kullanıcı bulunduysa
+                    {
+                        var newStudent = new students
+                        {
+                            email = recipient,
+                            password = randomNumber.ToString(),
+                            is_visible = true,
+                        };
+
+                        db.students.Add(newStudent); // Öğrenciyi veritabanına ekle
+                        db.SaveChanges(); // Değişiklikleri kaydet
+                       
+                    }
+    
+                }
+            }
+            catch (Exception ex)
+            {
+                // Hata durumunda kullanıcıya bilgi verme
+                lblMessage.Text = "E-posta gönderirken bir hata oluştu: " + ex.Message;
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+
+    }
+}
